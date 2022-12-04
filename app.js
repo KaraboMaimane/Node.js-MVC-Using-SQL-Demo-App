df3ab836029b27739c91596a3bc56f8ed77b9eba
KaraboMaimane <maimane23@hotmail.com>
2022-12-04 21:40:59 +0200

    Setting up MongoDB instance and Getting products

--- app.js
+++ app.js
@@ -4,28 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-
-const sequelize = require('./util/database');
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
-
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order')
-const OrderItem = require('./models/order-item')
-
-Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, {through: CartItem});
-Product.belongsToMany(Cart, {through: CartItem});
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, {through: OrderItem});
-
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -37,12 +18,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findByPk(1).then(user => {
-        req.user = user;
-        next(
+    // User.findByPk(1).then(user => {
+    //     req.user = user;
+    //     next(
 
-        );
-    }).catch(err => console.log(err));
+    //     );
+    // }).catch(err => console.log(err));
+    next();
 })
 
 app.use('/admin', adminRoutes);
@@ -50,24 +32,7 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize
-    .sync()
-    // .sync({force: true})
-    .then(result => {
-    return User.findByPk(1)
-}).then(user => {
-    if(!user){
-        User.create({username: 'Karabo', email: 'test@test.com'})
-    }
-    return Promise.resolve(user);
-}).then(
-    user => {
-        // console.log(User)
-       return user.createCart();
-    }
-).then(
-    cart => {
-        app.listen(3000);
-    }
-).catch(err => console.log('database connection error: ', err))
-////// 0012
+mongoConnect(() => {
+    app.listen(3000)
+})
+////// 0012 we going to 13
